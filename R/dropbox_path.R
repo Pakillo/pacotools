a9f83549a4af956b79b71803ea005ce1c01b8c22
Pakillo <f.rodriguez.sanc@gmail.com>
2015-06-15 18:54:08 +0200

    new, simplified dropbox_path

--- R/dropbox_path.R
+++ R/dropbox_path.R
@@ -1,15 +1,22 @@
 #' Get path to local Dropbox folder.
 #'
-#' This function returns the full path to the Dropbox folder in the local system. Currently it works for Windows only.
-#' 
-#' @importFrom RCurl base64
+#' This function returns the full path to the Dropbox folder in the local system.
+#'
 #' @export
+#' @importFrom stringr str_split
 #' @return A character vector with the path to the Dropbox folder.
-#' @author Petr Simecek
-#' @seealso \link{http://applyr.blogspot.com.es/2012/08/get-path-to-your-dropbox-folder.html}.
+
 dropbox_path <- function() {
-  if (Sys.info()["sysname"]!="Windows") stop("Currently, 'dropbox_path' works for Windows only. Sorry.")
-  db.file <- paste(Sys.getenv('APPDATA'), '\\Dropbox\\host.db', sep='')
-  base64coded <- readLines(db.file, warn=FALSE)[2]
-  base64(base64coded, encode=FALSE)
-} 
+  
+  # old version:
+  #   if (Sys.info()["sysname"]!="Windows") stop("Currently, 'dropbox_path' works for Windows only. Sorry.")
+  #   db.file <- paste(Sys.getenv('APPDATA'), '\\Dropbox\\host.db', sep='')
+  #   base64coded <- readLines(db.file, warn=FALSE)[2]
+  #   base64(base64coded, encode=FALSE)
+  
+  docs_path <- unlist(stringr::str_split(path.expand("~/"), "/"))
+  home_path <- paste(docs_path[1:3], collapse = "/")
+  dropbox_path <- paste(home_path, "Dropbox", sep = "/")
+  dropbox_path
+  
+}
